4b3377907110fedb9b5d588abb31771bc060ba66
Christalin Dorsey <christalin.dorsey03@gmail.com>
2026-03-14 23:13:30 -0700

    Initialize Next.JS Project

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+const config = {
+  bracketSpacing: true,
+  printWidth: 80,
+  quoteProps: 'as-needed',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+};
+
+export default config;
